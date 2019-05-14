ba37cc98e11c6a913defbed14ab36d831feb6700
allan <gjbae1212@gmail.com>
2019-05-14 17:28:42 +0900

    Add test codes of the count api.

--- handler/api/count.go
+++ handler/api/count.go
@@ -1,36 +1,146 @@
 package api_handler
 
 import (
+	"github.com/gjbae1212/go-badge"
 	"net/http"
 
 	"time"
 
 	"fmt"
 
-	"github.com/gjbae1212/go-badge"
+	"context"
+
+	"github.com/cespare/xxhash"
+	"github.com/gjbae1212/hit-counter/counter"
 	"github.com/gjbae1212/hit-counter/handler"
+	"github.com/gjbae1212/hit-counter/sentry"
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	badgeFormat = " %d / %d "
+	badgeFormat   = " %d / %d "
+	countIdFormat = "%s/%s"
+
+	domainGroup = "domain"
 )
 
-// TODO: 어뷰징 체크 (freecache 이용 xxhash로 저장)
-// TODO: cache 옵션 끄기, content type 정리
+type RankTask struct {
+	Counter   counter.Counter
+	Domain    string
+	Path      string
+	CreatedAt time.Time
+}
+
+func (task *RankTask) Process(ctx context.Context) error {
+	// If a domain is 'github.com', it is calculating ranks.
+	if task.Domain == "github.com" && task.Path != "" {
+		if _, err := task.Counter.IncreaseRankOfDaily(task.Domain, task.Path, task.CreatedAt); err != nil {
+			return err
+		}
+		if _, err := task.Counter.IncreaseRankOfTotal(task.Domain, task.Path); err != nil {
+			return err
+		}
+	}
+
+	// Calculate ranks for a domain of daily and total
+	if _, err := task.Counter.IncreaseRankOfDaily(domainGroup, task.Domain, task.CreatedAt); err != nil {
+		return err
+	}
+	if _, err := task.Counter.IncreaseRankOfTotal(domainGroup, task.Domain); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (h *Handler) IncrCount(c echo.Context) error {
-	return c.String(http.StatusOK, "health check!")
+	hctx := c.(*handler.HitCounterContext)
+	if hctx.Get("ckid") == nil || hctx.Get("host") == nil || hctx.Get("path") == nil {
+		return fmt.Errorf("[err] IncrCount empty params")
+	}
+	cookie := hctx.Get("ckid").(string)
+	host := hctx.Get("host").(string)
+	path := hctx.Get("path").(string)
+	_ = cookie
+	id := fmt.Sprintf(countIdFormat, host, path)
+	ip := c.RealIP()
+	userAgent := c.Request().UserAgent()
+
+	// If a ingress specified ip is exceeded more than 30 per 5 seconds, it might possibly abusing.
+	// so it must be limited.
+	v, ok := h.LocalCache.Get(ip)
+	if v != nil && v.(int64) > 20 {
+		daily, total, err := h.Counter.GetHitAll(id, time.Now())
+		if err != nil {
+			return err
+		}
+		return h.returnCount(hctx, daily, total)
+	}
+	if !ok {
+		h.LocalCache.Set(ip, int64(1), 5*time.Second)
+	} else {
+		if _, err := h.LocalCache.IncrementInt64(ip, 1); err != nil {
+			return err
+		}
+	}
+
+	// It would limit for count a hit when a user is accessing more than 1 per second.
+	temporaryId := fmt.Sprintf("%d", xxhash.Sum64String(fmt.Sprintf("%s-%s", ip, userAgent)))
+	if _, ok := h.LocalCache.Get(temporaryId); ok {
+		daily, total, err := h.Counter.GetHitAll(id, time.Now())
+		if err != nil {
+			return err
+		}
+		return h.returnCount(hctx, daily, total)
+	}
+	h.LocalCache.Set(temporaryId, int64(1), 1*time.Second)
+
+	daily, err := h.Counter.IncreaseHitOfDaily(id, time.Now())
+	if err != nil {
+		return err
+	}
+
+	total, err := h.Counter.IncreaseHitOfTotal(id)
+	if err != nil {
+		return err
+	}
+
+	// Calculating ranks of daily and total is asynchronously working.
+	timectx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := h.AsyncTask.AddTask(timectx, &RankTask{
+		Counter:   h.Counter,
+		Domain:    host,
+		Path:      path,
+		CreatedAt: time.Now(),
+	}); err != nil { // Possibly send an error to the sentry. And it is not returned a error.
+		sentry.SendSentry(err, nil)
+	}
+
+	return h.returnCount(hctx, daily, total)
 }
 
 func (h *Handler) KeepCount(c echo.Context) error {
 	hctx := c.(*handler.HitCounterContext)
-	id := hctx.Get("id").(string)
+	if hctx.Get("ckid") == nil || hctx.Get("host") == nil || hctx.Get("path") == nil {
+		return fmt.Errorf("[err] KeepCount empty params")
+	}
+	host := hctx.Get("host").(string)
+	path := hctx.Get("path").(string)
+	cookie := hctx.Get("ckid").(string)
+	_ = cookie
+	id := fmt.Sprintf(countIdFormat, host, path)
 
 	daily, total, err := h.Counter.GetHitAll(id, time.Now())
 	if err != nil {
 		return err
 	}
 
+	return h.returnCount(hctx, daily, total)
+}
+
+func (h *Handler) returnCount(hctx *handler.HitCounterContext, daily, total *counter.Score) error {
 	dailyCount := int64(0)
 	totalCount := int64(0)
 	if daily != nil {
@@ -46,9 +156,9 @@ func (h *Handler) KeepCount(c echo.Context) error {
 		return err
 	}
 
-	c.Response().Header().Set("Content-Type", "image/svg+xml")
-	c.Response().Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-	c.Response().Header().Set("Pragma", "no-cache")
-	c.Response().Header().Set("Expires", "0")
-	return c.String(http.StatusOK, string(badge))
+	hctx.Response().Header().Set("Content-Type", "image/svg+xml")
+	hctx.Response().Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	hctx.Response().Header().Set("Pragma", "no-cache")
+	hctx.Response().Header().Set("Expires", "0")
+	return hctx.String(http.StatusOK, string(badge))
 }
--- handler/api/count_test.go
+++ handler/api/count_test.go
@@ -1,2 +1,221 @@
 package api_handler
 
+import (
+	"github.com/gjbae1212/go-badge"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+
+	"context"
+
+	"fmt"
+
+	"github.com/alicebob/miniredis"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gjbae1212/hit-counter/counter"
+	"github.com/gjbae1212/hit-counter/handler"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRankTask_Process(t *testing.T) {
+	assert := assert.New(t)
+
+	s, err := miniredis.Run()
+	assert.NoError(err)
+	defer s.Close()
+
+	h, err := handler.NewHandler([]string{s.Addr()})
+	assert.NoError(err)
+
+	api, err := NewHandler(h)
+	assert.NoError(err)
+
+	tests := map[string]struct {
+		input *RankTask
+		wants []*counter.Score
+	}{
+		"not_github": {input: &RankTask{
+			Counter:   h.Counter,
+			Domain:    "allan.com",
+			Path:      "aa/bb",
+			CreatedAt: time.Now(),
+		}, wants: []*counter.Score{
+			nil,
+			nil,
+			&counter.Score{
+				Name:  "allan.com",
+				Value: 1,
+			},
+			&counter.Score{
+				Name:  "allan.com",
+				Value: 1,
+			},
+		}},
+		"github": {input: &RankTask{
+			Counter:   h.Counter,
+			Domain:    "github.com",
+			Path:      "gjbae1212/test",
+			CreatedAt: time.Now(),
+		}, wants: []*counter.Score{
+			&counter.Score{
+				Name:  "gjbae1212/test",
+				Value: 1,
+			},
+			&counter.Score{
+				Name:  "gjbae1212/test",
+				Value: 1,
+			},
+			&counter.Score{
+				Name:  "github.com",
+				Value: 1,
+			},
+			&counter.Score{
+				Name:  "github.com",
+				Value: 1,
+			},
+		}},
+	}
+
+	ctx := context.Background()
+
+	test := tests["not_github"]
+	err = api.AsyncTask.AddTask(ctx, test.input)
+	assert.NoError(err)
+	time.Sleep(1 * time.Second)
+	scores, err := api.Counter.GetRankDailyByLimit(test.input.Domain, 10, time.Now())
+	assert.NoError(err)
+	assert.Len(scores, 0)
+	scores, err = api.Counter.GetRankTotalByLimit(test.input.Domain, 10)
+	assert.NoError(err)
+	assert.Len(scores, 0)
+	scores, err = api.Counter.GetRankDailyByLimit("domain", 10, time.Now())
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.True(cmp.Equal(test.wants[2], scores[0]))
+
+	scores, err = api.Counter.GetRankTotalByLimit("domain", 10)
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.True(cmp.Equal(test.wants[3], scores[0]))
+
+	test = tests["github"]
+	err = api.AsyncTask.AddTask(ctx, test.input)
+	assert.NoError(err)
+	time.Sleep(1 * time.Second)
+	scores, err = api.Counter.GetRankDailyByLimit(test.input.Domain, 10, time.Now())
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.True(cmp.Equal(test.wants[0], scores[0]))
+	spew.Dump(scores)
+
+	scores, err = api.Counter.GetRankTotalByLimit(test.input.Domain, 10)
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.True(cmp.Equal(test.wants[1], scores[0]))
+	spew.Dump(scores)
+
+	scores, err = api.Counter.GetRankDailyByLimit("domain", 10, time.Now())
+	assert.NoError(err)
+	assert.Len(scores, 2)
+	if cmp.Equal(test.wants[2], scores[0]) {
+	} else if cmp.Equal(test.wants[2], scores[1]) {
+	} else {
+		assert.NoError(fmt.Errorf("error"))
+	}
+
+	scores, err = api.Counter.GetRankTotalByLimit("domain", 10)
+	assert.NoError(err)
+	assert.Len(scores, 2)
+	if cmp.Equal(test.wants[3], scores[0]) {
+	} else if cmp.Equal(test.wants[3], scores[1]) {
+	} else {
+		assert.NoError(fmt.Errorf("error"))
+	}
+}
+
+func TestHandler_KeepCount(t *testing.T) {
+	assert := assert.New(t)
+
+	s, err := miniredis.Run()
+	assert.NoError(err)
+	defer s.Close()
+
+	h, err := handler.NewHandler([]string{s.Addr()})
+	assert.NoError(err)
+
+	api, err := NewHandler(h)
+	assert.NoError(err)
+
+	e := echo.New()
+	r := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	w := httptest.NewRecorder()
+	hctx := &handler.HitCounterContext{Context: e.NewContext(r, w)}
+	err = api.KeepCount(hctx)
+	assert.Error(err)
+
+	hctx.Set("ckid", "test")
+	hctx.Set("host", "github.com")
+	hctx.Set("path", "gjbae1212/hit-counter")
+	err = api.KeepCount(hctx)
+	assert.NoError(err)
+	assert.Equal(200, w.Code)
+	text := fmt.Sprintf(badgeFormat, 0, 0)
+	badge, err := badge.RenderBytes("hits", text, "#79c83d")
+	assert.NoError(err)
+	assert.Equal(string(badge), w.Body.String())
+}
+
+func TestHandler_IncrCount(t *testing.T) {
+	assert := assert.New(t)
+
+	s, err := miniredis.Run()
+	assert.NoError(err)
+	defer s.Close()
+
+	h, err := handler.NewHandler([]string{s.Addr()})
+	assert.NoError(err)
+
+	api, err := NewHandler(h)
+	assert.NoError(err)
+
+	e := echo.New()
+	r := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	w := httptest.NewRecorder()
+	hctx := &handler.HitCounterContext{Context: e.NewContext(r, w)}
+	err = api.IncrCount(hctx)
+	assert.Error(err)
+
+	for i := 1; i < 20; i++ {
+		r := httptest.NewRequest("GET", "http://localhost:8080", nil)
+		r.Header.Set("User-Agent", fmt.Sprintf("%d", i))
+		w := httptest.NewRecorder()
+		hctx := &handler.HitCounterContext{Context: e.NewContext(r, w)}
+		hctx.Set("ckid", "test")
+		hctx.Set("host", "github.com")
+		hctx.Set("path", "gjbae1212/hit-counter")
+		err = api.IncrCount(hctx)
+		assert.NoError(err)
+		assert.Equal(200, w.Code)
+		text := fmt.Sprintf(badgeFormat, i, i)
+		badge, err := badge.RenderBytes("hits", text, "#79c83d")
+		assert.NoError(err)
+		assert.Equal(string(badge), w.Body.String())
+	}
+	time.Sleep(1 * time.Second)
+	scores, err := api.Counter.GetRankDailyByLimit("github.com", 10, time.Now())
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.Equal(int64(19), scores[0].Value)
+	scores, err = api.Counter.GetRankTotalByLimit("github.com", 10)
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.Equal(int64(19), scores[0].Value)
+	scores, err = api.Counter.GetRankTotalByLimit("domain", 10)
+	assert.NoError(err)
+	assert.Len(scores, 1)
+	assert.Equal(int64(19), scores[0].Value)
+
+}
