b673c42172530f2c24b9ef4c4a2951034602c0f2
JJD <jungdujang@gmail.com>
2020-08-23 13:29:32 +0900

    Update: Add script for restarting dev server when it detects changes in the file. (#14)

---
+++ script/dev_server_restarter.js
@@ -0,0 +1,46 @@
+const { spawnSync, spawn } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const env = {
+	PHASE: "local",
+	REDIS_ADDRS: "localhost:6379",
+};
+
+function throttle(fn, duration) {
+	return (function () {
+		let timeout;
+		return function (...args) {
+			if (!timeout) {
+				fn(...args);
+				timeout = setTimeout(() => {
+					timeout = null;
+				}, duration);
+			}
+		};
+	})();
+}
+
+function runServer() {
+	spawnSync("go build", { stdio: "inherit", env: { ...process.env, ...env } });
+	return spawn("./hit-counter", ["-tls=0", "-addr=:8080"], {
+		stdio: "inherit",
+		env: { ...process.env, ...env },
+	});
+}
+
+function main() {
+	let devServer;
+	devServer = runServer();
+	fs.watch(
+		path.join(__dirname, "../view"),
+		throttle(() => {
+			if (devServer) {
+				devServer.kill();
+			}
+			devServer = runServer();
+		}, 1000)
+	);
+}
+
+main();
